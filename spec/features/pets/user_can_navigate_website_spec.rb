cd978c52e3a743a256ae0cd676041658db4f8c50
“Krista <stadler.krista@gmail..com>
2020-03-21 13:04:35 -0600

    Added functionality to update shelter from shelter index page

---
+++ spec/features/pets/user_can_navigate_website_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "when user visits the shelter index page", type: :feature do
+  it "can click a link to edit the shelters information" do
+    shelter_1 = Shelter.create(name:    "Longmont Humane Society",
+                               address: "9595 Nelson Road",
+                               city:    "Longmont",
+                               state:   "Colorado",
+                               zip:     "80501")
+    shelter_2 = Shelter.create(name:    "Humane Society of Boulder Valley",
+                               address: "2323 55th St",
+                               city:    "Boulder",
+                               state:   "Colorado",
+                               zip:     "80301")
+    shelter_3 = Shelter.create(name:    "Adams County Animal Services",
+                              address: "10705 Fulton St",
+                              city:    "Brighton",
+                              state:   "Colorado",
+                              zip:     "80601")
+
+    visit "/shelters"
+
+    expect(page).to have_link 'Edit Shelter', href: "/shelters/#{shelter_1.id}/edit"
+    expect(page).to have_link 'Edit Shelter', href: "/shelters/#{shelter_2.id}/edit"
+    expect(page).to have_link 'Edit Shelter', href: "/shelters/#{shelter_3.id}/edit"
+  end
+end   
